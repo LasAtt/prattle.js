32c526edcc485cd1727d8bbf72d26f20de3bbc8e
Atte Lassila <atte.lassila@cs.helsinki.fi>
2017-02-06 23:27:47 +0200

    more deployment debugging, sockets being difficult now

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,6 +1,7 @@
 module.exports = (io, db) =>{
   return (socket) => {
     socket.on('message:new', (message) => {
+      console.log('message');
       db.get('messages').insert(message).then((res) => {
           io.sockets.emit('message:new', res);
       });
